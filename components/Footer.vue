7dd81582a8c77aeb1046731c0db2b91ab2c622ef
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2022-02-05 18:31:27 +0300

    footer + new page

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer height ="50" color="orange">
+
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: `Footer`
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,27 +10,33 @@
           <v-btn  class="mb-5">
             <nuxt-link to="/about" class="black--text">О нас</nuxt-link>
           </v-btn>
+          <v-btn  class="mb-5">
+            <nuxt-link to="/events" class="black--text">События</nuxt-link>
+          </v-btn>
         </nav>
         <div class="mt-10">
           <p class="text-center ml-5">Зарегистрировано погодных событий: 
             <span class="font-weight-bold h3">{{ eventsCount.events }}</span>
           </p>
-          <v-img :src="require('~/static/left_widget_img.jpg')" width="285px" height="190px" class="ml-5"></v-img>
+          <v-img :src="require('~/static/left_widget_img.jpg')" width="250px" height="190px" class="ml-15"></v-img>
         </div>
       </v-col>
       <v-col class="col-10 col-sm-6 col-md-8">
         <nuxt />
       </v-col>
     </v-row>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import Header from "@/components/Header"
+import Footer from '~/components/Footer.vue'
 
 export default {
   name: 'DefaultLayout',
-  components: { Header },
+  components: { Header,
+  Footer },
   data: () => ({
     eventsCount: []
   }),
